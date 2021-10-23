e29ac07a0e6645e30a77a86b84c108a3dead6b56
hernan3396 <hernan3396@gmail.com>
2021-10-23 14:09:27 -0300

    added biniding loading

---
+++ Assets/Scripts/Menu/LoadBindings.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LoadBindings : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset actionsAsset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (InputActionMap item in actionsAsset.actionMaps)
+        {
+            // Debug.Log(item.ToJson());
+            // actionMaps.Add(item);
+            foreach (InputAction action in item.actions)
+            {
+                string name = "CO" + action.bindings[0].action;
+                if (PlayerPrefs.HasKey(name))
+                {
+                    InputBinding binding = action.bindings[0];
+                    binding.overridePath = PlayerPrefs.GetString(name);
+                    action.ApplyBindingOverride(0, binding);
+                }
+                else
+                {
+                    PlayerPrefs.SetString(name, action.bindings[0].effectivePath);
+                }
+            }
+        }
+
+    }
+}
