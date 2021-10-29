76541c94b01c9c30c62a23dba44cf8a4dc52236c
hernan3396 <hernan3396@gmail.com>
2021-10-29 10:44:18 -0300

    added bullet duration

--- Assets/Scripts/Enemies/Bullet.cs
+++ Assets/Scripts/Enemies/Bullet.cs
@@ -1,11 +1,9 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [Range(1, 100)] [SerializeField] private float speed = 10;
     float duration = 2;
 
     void OnCollisionEnter2D(Collision2D collision) //Si colisiona con cualquier box collider va a desaparecer, ojito
@@ -13,9 +11,14 @@ public class Bullet : MonoBehaviour
         this.gameObject.SetActive(false);
     }
 
-    void Update()
+    private void OnEnable()
     {
-        // esto lo correcto seria hacerlo en el start con un velocity o algo de eso, pero para probar rinde
-       // transform.position = Vector2.MoveTowards(transform.position, target.position, speed);
+        StartCoroutine("Despawn");
+    }
+
+    IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(duration);
+        this.gameObject.SetActive(false);
     }
 }
